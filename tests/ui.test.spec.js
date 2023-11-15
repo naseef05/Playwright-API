7f31e0b3fd1caf52810c67eedc2ba691e6a64c5d
Naseef <naseef.abdullah@gainsco.com>
2023-11-14 21:30:38 -0600

    new scenarios

--- tests/ui.test.spec.js
+++ tests/ui.test.spec.js
@@ -48,7 +48,44 @@ test.describe('Make Five legal move', () => {
         await page.locator("xpath=//*[@name='space13']").click();
         await page.waitForTimeout(2000);
         await page.locator("xpath=//*[@name='space35']").click();
+        await page.waitForTimeout(5000);
+        //count the blue chip number is 11 since one is taken
+        const autocompleteItem = page.locator("xpath=//*[@src='me1.gif']");
+            const count = await autocompleteItem.count()
+            console.log(count)
+            expect(count).toEqual(11)
+      });
+      test('Restart the game after five moves', async ({ page }) => {
+        test.setTimeout(520000);
+        // create a new todo locator
+        await expect(page).toHaveTitle(/Checkers - Games for the Brain/);
+        await page.locator("xpath=//*[@name='space62']").click();
         await page.waitForTimeout(2000);
+        await page.locator("xpath=//*[@name='space53']").click();
+        await page.waitForTimeout(2000);
+        await expect(page.locator("//*[@id='message']")).toHaveText('Make a move.');
+        await page.locator("xpath=//*[@name='space53']").click();
+        await page.waitForTimeout(2000);
+        await page.locator("xpath=//*[@name='space64']").click();
+        await page.waitForTimeout(2000);
+        await expect(page.locator("//*[@id='message']")).toHaveText('Make a move.');
+        await page.locator("xpath=//*[@name='space22']").click();
+        await page.waitForTimeout(2000);
+        await page.locator("xpath=//*[@name='space13']").click();
+        await page.waitForTimeout(2000);
+        await expect(page.locator("//*[@id='message']")).toHaveText('Make a move.');
+        await page.locator("xpath=//*[@name='space13']").click();
+        await page.waitForTimeout(2000);
+        await page.locator("xpath=//*[@name='space35']").click();
+        await page.waitForTimeout(2000);
+        await page.locator("xpath=//*[@name='space11']").click();
+        await page.waitForTimeout(2000);
+        await page.locator("xpath=//*[@name='space22']").click();
+        await page.waitForTimeout(2000);
+        await page.getByText('Restart...').click();
+        await page.waitForTimeout(2000);
+        await expect(page.locator("//*[@id='message']")).toHaveText('Select an orange piece to move.');
+       
       });
 
 })
